0a474bfeb3eeec99982cce92cf5352cc6bb25b1a
czengg <czeng.andrew.cmu.edu>
2012-12-10 04:45:11 -0500

    touch vs tap

--- public/googleDocs.js
+++ public/googleDocs.js
@@ -47,22 +47,22 @@ function handleAuthResult(authResult) {
     console.log(gapi);
     getId(authResult);
   } else {
-    $("#logOutButton").css("display","none");
+    // $("#logOutButton").css("display","none");
     // No access token could be retrieved, show the button to start the authorization flow.
     authButton.css('inline-block');
     $(".button-home-notes").addClass("ui-disabled");
     authButton.click(function() {
         enableButtons();
         authButton.css("display","none");
-        $("#logOutButton").css("display","inline-block");
+        // $("#logOutButton").css("display","inline-block");
         gapi.auth.authorize(
             {'client_id': CLIENT_ID, 'scope': SCOPES, 'immediate': false},
             handleAuthResult);
     });
     authButton.on('tap', function() {
         enableButtons();
-        authButton.css("display","none");
-        $("#logOutButton").css("display","inline-block");
+        // authButton.css("display","none");
+        // $("#logOutButton").css("display","inline-block");
         gapi.auth.authorize(
             {'client_id': CLIENT_ID, 'scope': SCOPES, 'immediate': false},
             handleAuthResult);
